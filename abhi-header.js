db34bd2f1d29f788810953575b23e2541f4a57a0
abhinavpb-249 <abhinavpb249@gmail.com>
2023-06-01 10:18:27 +0530

    initial commit

---
+++ abhi-header.js
@@ -0,0 +1,54 @@
+
+
+let appHeader = `    
+
+	<title>CU Bsc Physics</title>
+	<link rel = "icon" href = "media/channel_logo.png"  type = "image/x-icon">
+	<nav>
+	    <img id="logo" src="media/channel_logo.png" />
+	    <button id="hamburger-icon"><span class="hamburger-line"></span><span class="hamburger-line"></span><span class="hamburger-line"></span></button>
+            <ul class="menu">
+                <li> <div  id ="index" onclick='goto(this.id)'>     Home</div></li>
+                <li> <div id="Textbooks" onclick='goto(this.id)'>   Github</div></li>
+                <li> <div id="classes" onclick='goto(this.id)'>     Linkedin</div></li>                
+                <li> <div id="experiments" onclick='goto(this.id)'> Projects</div></li>
+                
+            </ul>
+        </nav>
+`;
+
+document.getElementById("app-header").innerHTML = appHeader;
+
+
+
+//get the name of the current page
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var name = page.split(".")[0];
+console.log( name );
+//get the button with that name
+var Button = document.getElementById(name);
+//change that color of the button
+Button.style.border="3px solid black";
+
+
+
+
+ function goto(buttonID){
+        file_name =buttonID+".html"	
+	window.open(file_name,"_self")
+}
+
+  const hamburgerIcon = document.getElementById('hamburger-icon');
+  const menu = document.getElementsByClassName('menu')[0];
+
+ 
+hamburgerIcon.addEventListener('click', function() {
+  const computedStyle = getComputedStyle(menu);
+  if (computedStyle.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
+
