fc1d3bd859cdad366841faaf16fc32cbcb4a5be7
abhinavpb-249 <abhinavpb249@gmail.com>
2023-06-10 20:27:55 +0530

    initial commit

--- js/header.js
+++ js/header.js
@@ -1,5 +1,3 @@
-
-
 let appHeader = `    
 	
 	<link rel = "icon" href = "media/channel_logo.png"  type = "image/x-icon">
@@ -7,7 +5,7 @@ let appHeader = `
 
 	<nav>
 	    <img id="logo" src="media/thesophile_logo.png" />
-	    <button id="hamburger-icon"><span class="hamburger-line"></span><span class="hamburger-line"></span><span class="hamburger-line"></span></button>
+	    <button id="hamburger-icon" onclick="showMenu()"><span class="hamburger-line"></span><span class="hamburger-line"></span><span class="hamburger-line"></span></button>
             <ul class="menu">
                 <li> <div  id ="index" onclick='goto(this.id)'>     Home</div></li>
                 <li> <div id="contact" onclick='goto(this.id)'> Contact</div></li>                
@@ -32,7 +30,6 @@ Button.style.border="3px solid black";
 
 
 
-
  function goto(buttonID){
         file_name =buttonID+".html"	
 	window.open(file_name,"_self")
@@ -40,14 +37,28 @@ Button.style.border="3px solid black";
 
   const hamburgerIcon = document.getElementById('hamburger-icon');
   const menu = document.getElementsByClassName('menu')[0];
+  
+  function showMenu() {
+  
+  
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+  
+  }
+  
 
- 
+ /*
 hamburgerIcon.addEventListener('click', function() {
-
+  
+  
   if (menu.style.display === "none") {
     menu.style.display = "block";
   } else {
     menu.style.display = "none";
   }
 });
+*/
 
